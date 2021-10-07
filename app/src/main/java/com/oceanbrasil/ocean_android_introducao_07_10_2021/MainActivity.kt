d46731d667cfcffe13cf3720395594f5e6bacc4f
Bruno Cavalcante <brian.solidus@gmail.com>
2021-10-07 12:54:50 -0300

    add reset button, input text field

--- app/src/main/java/com/oceanbrasil/ocean_android_introducao_07_10_2021/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_introducao_07_10_2021/MainActivity.kt
@@ -2,10 +2,33 @@ package com.oceanbrasil.ocean_android_introducao_07_10_2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Buscar o botão pelo ID
+        val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val btReset = findViewById<Button>(R.id.btReset)
+        val editTextTextPersonName = findViewById<TextView>(R.id.editTextTextPersonName)
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+        btEnviar.setOnClickListener {
+            val personName = editTextTextPersonName.text
+            tvResultado.text = personName
+        }
+
+        btReset.setOnClickListener {
+            editTextTextPersonName.text = resources.getString(R.string.digite_o_seu_nome)
+            tvResultado.text = resources.getString(R.string.o_resultado_aparecer_aqui)
+        }
+
+        /*
+        Exercício: Construir um elemento de EditText no layout (entre o Textview e o Botão)
+        Ao clicar no botão, pegar o que foi digitado no EditText e atualizar no text do Textview
+         */
     }
 }
